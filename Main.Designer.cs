1710aaa19ccc4346feec55341f14764e3329c5e8
alastair.crabtree <alastair.crabtree@bbc.com>
2014-10-06 18:01:30 +0100

    Basic design of app complete

---
+++ Main.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace SqlToFileCopy
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogTable = new System.Windows.Forms.DataGridView();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ConnectionStringTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.QueryTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DestinationFolderTextBox = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.CopyFilesButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LogTable)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LogTable
+            // 
+            this.LogTable.AllowUserToAddRows = false;
+            this.LogTable.AllowUserToDeleteRows = false;
+            this.LogTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LogTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Time,
+            this.Message});
+            this.tableLayoutPanel1.SetColumnSpan(this.LogTable, 2);
+            this.LogTable.Location = new System.Drawing.Point(103, 273);
+            this.LogTable.Name = "LogTable";
+            this.LogTable.ReadOnly = true;
+            this.LogTable.Size = new System.Drawing.Size(479, 150);
+            this.LogTable.TabIndex = 10;
+            // 
+            // Time
+            // 
+            this.Time.DataPropertyName = "Time";
+            this.Time.HeaderText = "Time";
+            this.Time.Name = "Time";
+            this.Time.ReadOnly = true;
+            // 
+            // Message
+            // 
+            this.Message.DataPropertyName = "Message";
+            this.Message.HeaderText = "Message";
+            this.Message.Name = "Message";
+            this.Message.ReadOnly = true;
+            this.Message.Width = 300;
+            // 
+            // ConnectionStringTextBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.ConnectionStringTextBox, 2);
+            this.ConnectionStringTextBox.Location = new System.Drawing.Point(103, 3);
+            this.ConnectionStringTextBox.Name = "ConnectionStringTextBox";
+            this.ConnectionStringTextBox.Size = new System.Drawing.Size(479, 20);
+            this.ConnectionStringTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection string";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(418, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SqlToFileCopy queries a list of files from your database and copies them to a des" +
+    "tination";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.3629F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.6371F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 91F));
+            this.tableLayoutPanel1.Controls.Add(this.ConnectionStringTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.QueryTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.DestinationFolderTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BrowseButton, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.CopyFilesButton, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.LogTable, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 29);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.30043F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.69957F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(586, 451);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Files list query";
+            // 
+            // QueryTextBox
+            // 
+            this.QueryTextBox.AcceptsReturn = true;
+            this.QueryTextBox.AcceptsTab = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.QueryTextBox, 2);
+            this.QueryTextBox.HideSelection = false;
+            this.QueryTextBox.Location = new System.Drawing.Point(103, 24);
+            this.QueryTextBox.Multiline = true;
+            this.QueryTextBox.Name = "QueryTextBox";
+            this.QueryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.QueryTextBox.Size = new System.Drawing.Size(479, 150);
+            this.QueryTextBox.TabIndex = 3;
+            this.QueryTextBox.Text = "SELECT FilePath FROM [dbo].[FilesTable]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Copy destination";
+            // 
+            // DestinationFolderTextBox
+            // 
+            this.DestinationFolderTextBox.Location = new System.Drawing.Point(103, 209);
+            this.DestinationFolderTextBox.Name = "DestinationFolderTextBox";
+            this.DestinationFolderTextBox.Size = new System.Drawing.Size(388, 20);
+            this.DestinationFolderTextBox.TabIndex = 6;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(497, 209);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(84, 26);
+            this.BrowseButton.TabIndex = 7;
+            this.BrowseButton.Text = "Select Folder";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CopyFilesButton
+            // 
+            this.CopyFilesButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.CopyFilesButton.Location = new System.Drawing.Point(497, 241);
+            this.CopyFilesButton.Name = "CopyFilesButton";
+            this.CopyFilesButton.Size = new System.Drawing.Size(84, 26);
+            this.CopyFilesButton.TabIndex = 8;
+            this.CopyFilesButton.Text = "Copy files";
+            this.CopyFilesButton.UseVisualStyleBackColor = false;
+            this.CopyFilesButton.Click += new System.EventHandler(this.CopyFilesButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Copy log";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 521);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Name = "Main";
+            this.Text = "SqlToFileCopy";
+            ((System.ComponentModel.ISupportInitialize)(this.LogTable)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ConnectionStringTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox QueryTextBox;
+        private System.Windows.Forms.TextBox DestinationFolderTextBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button CopyFilesButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
+        private System.Windows.Forms.DataGridView LogTable;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlToFileCopy
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            LogData = new List<object>();
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // New FolderBrowserDialog instance
+            FolderBrowserDialog Fld = new FolderBrowserDialog();
+
+            // Set initial selected folder
+            Fld.SelectedPath = DestinationFolderTextBox.Text;
+
+            // Show the "Make new folder" button
+            Fld.ShowNewFolderButton = true;
+
+            if (Fld.ShowDialog() == DialogResult.OK)
+            {
+                // Select successful
+                DestinationFolderTextBox.Text = Fld.SelectedPath;
+            }
+        }
+
+        private void CopyFilesButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(ConnectionStringTextBox.Text))
+            {
+                WriteLog("Missing connection string");
+                return;
+            }
+            if (String.IsNullOrEmpty(QueryTextBox.Text))
+            {
+                WriteLog("Missing query");
+                return;
+            }
+            if (String.IsNullOrEmpty(DestinationFolderTextBox.Text))
+            {
+                WriteLog("Missing destination");
+                return;
+            }
+
+            var destination = new DirectoryInfo(DestinationFolderTextBox.Text);
+            if (!destination.Exists)
+            {
+                WriteLog("Destination does not exist");
+                return;
+            }
+
+
+        }
+
+        private void WriteLog(string msg)
+        {
+            LogData.Add(new{ Time = DateTime.Now.ToString("dd MMM hh:mm"), Message =  msg});
+            LogTable.DataSource = LogData;
+
+        }
+
+        public List<object> LogData
+        { get; set; }
+    }
+}
